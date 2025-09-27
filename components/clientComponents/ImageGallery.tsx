b1bab9c9337dce59004f4653d47f006140f45fce
Amosram <amosrama733@gmail.com>
2025-09-27 17:55:21 +0300

    gallery UI responsiveness

--- components/clientComponents/ImageGallery.tsx
+++ components/clientComponents/ImageGallery.tsx
@@ -84,7 +84,7 @@ export default function ImageGallery({ images, businessNumber }: ImageGalleryPro
   return (
     <div className="container mx-auto px-4 py-8">
       {/* Gallery Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {images.map((image, index) => (
           <div
             key={image.id}
